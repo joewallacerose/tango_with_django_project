f91029c2b12419881a8dbe241c071d669d8dc61a
Joe Rose <2565536r@student.gla.ac.uk>
2022-01-28 20:02:17 +0000

    Chapter 3 Tests Completing

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=context_dict)
+    return HttpResponse("Rango says hey there partner! <a href=\'/rango/about/\'>About</a> ")
 
-# Create your views here.
+#    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+#    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href=\'/rango/\'>Index</a>")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,6 +23,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rango import views
 
+
+
 urlpatterns = [
                   path('', views.index, name="index"),
                   path('rango/', include('rango.urls')),
