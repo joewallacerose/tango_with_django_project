6c25be6218a63c16ffb8c7fea7e99374f126a4f5
Joe Rose <2565536r@student.gla.ac.uk>
2022-01-29 15:50:31 +0000

    Chapter 3 Tests Completing

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,8 +16,11 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = MEDIA_DIR
 
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -126,5 +129,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_ROOT = '/Users/joewr/website_workspace/tango_with_django_project/'
+
